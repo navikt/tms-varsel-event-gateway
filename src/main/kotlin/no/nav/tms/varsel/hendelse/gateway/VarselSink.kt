013a9f8eb252c3ad27e7ad8ffe72137977dada6e
Christopher Santana <christopher.santana@nav.no>
2023-05-12 10:24:25 +0200

    Bruk reject i stedet for forbid.

--- src/main/kotlin/no/nav/tms/varsel/hendelse/gateway/VarselSink.kt
+++ src/main/kotlin/no/nav/tms/varsel/hendelse/gateway/VarselSink.kt
@@ -16,7 +16,7 @@ class VarselSink(
     init {
         River(rapidsConnection).apply {
             validate { it.demandAny("@event_name", listOf("aktivert", "inaktivert")) }
-            validate { it.forbidValues("@source", listOf("varsel-authority")) }
+            validate { it.rejectValue("@source", "varsel-authority") }
             validate {
                 it.requireKey("varselType")
                 it.requireKey("eventId")

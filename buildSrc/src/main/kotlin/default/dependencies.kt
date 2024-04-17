88880e7d3bda5d27e488dd2cdb2e77c2f370e40f
Dependency Admin <personbruker@nav.no>
2024-04-17 08:57:33 +0000

    Legger tilbake testutils.

--- buildSrc/src/main/kotlin/default/dependencies.kt
+++ buildSrc/src/main/kotlin/default/dependencies.kt
@@ -155,7 +155,7 @@ object KtorDefaults {
 
 interface LogstashDefaults: DependencyGroup {
     override val groupId get() = "net.logstash.logback"
-    override val version get() = "7.17.20"
+    override val version get() = "7.4"
 
     val logbackEncoder get() = dependency("logstash-logback-encoder")
 }
@@ -222,6 +222,7 @@ interface TmsCommonLibDefaults: DependencyGroup {
     val observability get() = dependency("observability")
     val utils get() = dependency("utils")
     val kubernetes get() = dependency("kubernetes")
+    val testutils get () = dependency("testutils")
 }
 
 interface TmsKafkaToolsDefaults: DependencyGroup {
